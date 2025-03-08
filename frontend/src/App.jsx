07db3c18ee6819284f27ba472ede42de6c99a537
shinasmhmd <112807814+shinasmhmd@users.noreply.github.com>
2025-03-08 23:22:39 +0530

    QuizPage.css and QuizPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Questions from './components/Quizpage'
 
 function App() {
 
   return (
     <>
-      
+      <Questions/>
     </>
   )
 }
